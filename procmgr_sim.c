b714efdb98eae2e03cbb80cc99bdb56ab7963778
JamWild96 <100748711+JamWild96@users.noreply.github.com>
2025-02-03 23:47:02 -0800

    Adding .c file

---
+++ procmgr_sim.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define NUM_CHILDREN 10
+
+void execute_task(int task_number) {
+    if (task_number == 0) {
+        // Task 1: Echo Hello <Your Name>
+        char *args[] = {"echo", "Hello <Your Name>", NULL};
+        execvp(args[0], args);
+    } else {
+        // Other tasks: Example commands (can be modified)
+        char *commands[][3] = {
+            {"ls", "-l", NULL},
+            {"pwd", NULL, NULL},
+            {"whoami", NULL, NULL},
+            {"date", NULL, NULL},
+            {"uptime", NULL, NULL},
+            {"ps", "aux", NULL},
+            {"df", "-h", NULL},
+            {"uname", "-a", NULL},
+            {"cal", NULL, NULL}
+        };
+
+        execvp(commands[task_number - 1][0], commands[task_number - 1]);
+    }
+    perror("execvp failed"); // If exec fails
+    exit(1);
+}
+
+int main() {
+    pid_t pid;
+    int i, status;
+
+    printf("Parent Process (PID: %d) creating child processes...\n", getpid());
+
+    for (i = 0; i < NUM_CHILDREN; i++) {
+        pid = fork();
+
+        if (pid < 0) {
+            perror("Fork failed");
+            exit(1);
+        } else if (pid == 0) {  // Child process
+            printf("Child %d (PID: %d) executing task %d\n", i + 1, getpid(), i);
+            execute_task(i);
+            exit(0);
+        }
+    }
+
+    // Parent process waits for all child processes
+    for (i = 0; i < NUM_CHILDREN; i++) {
+        pid_t child_pid = wait(&status);
+        if (WIFEXITED(status)) {
+            printf("Child process %d terminated normally with exit status %d\n", child_pid, WEXITSTATUS(status));
+        } else if (WIFSIGNALED(status)) {
+            printf("Child process %d terminated by signal %d\n", child_pid, WTERMSIG(status));
+        }
+    }
+
+    printf("All child processes completed.\n");
+    return 0;
+}
